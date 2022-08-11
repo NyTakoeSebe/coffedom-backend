d97f7c488942364413c478ff60727cab541e829c
ooo <ooo@ooo.ooo>
2022-08-11 21:00:42 +0700

    address & preset & promo: controller, route, model

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import mongoose from 'mongoose';
 import colors from 'colors';
 
 import productRoutes from './routes/productRoutes.js';
+import promoRoutes from './routes/promoRoutes.js';
+import addressRoutes from './routes/addressRoutes.js';
+import presetRoutes from './routes/presetRoutes.js';
 
 import { connectDB } from './db.js';
 
@@ -18,5 +20,8 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/products', productRoutes);
+app.use('/api/promos', promoRoutes);
+app.use('/api/addresses', addressRoutes);
+app.use('/api/presets', presetRoutes);
 
 app.listen(PORT, console.log(`Server started `.blue.bold));
